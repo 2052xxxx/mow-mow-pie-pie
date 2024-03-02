9b6175378d3a777224118e72996f792da76af25a
2052xxxx <20522039@gm.uit.edu.vn>
2024-03-02 18:09:50 +0700

    no update haizz

--- chapter2/chapter2_P-2.35/client.py
+++ chapter2/chapter2_P-2.35/client.py
@@ -6,24 +6,27 @@ PORT = 9999  # The port used by the server
 
 def client_program():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
+    try:
+        client_socket.connect((HOST, PORT))
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-    receive_thread.start()
-    while True:
-        try:
-            message = input()
-            while message.lower().strip() != 'bye':
-                client_socket.send(message.encode('utf-8'))
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+        receive_thread.start()
+        while True:
+            try:
                 message = input()
-            client_socket.send(message.encode('utf-8'))
-            break
-        except:
-            print("Connection to the server closed.")
-            break
-    client_socket.close()
-    receive_thread.join()
-
+                while message.lower().strip() != 'bye':
+                    client_socket.send(message.encode('utf-8'))
+                    message = input()
+                client_socket.send(message.encode('utf-8'))
+                break
+            except:
+                print("Closed the connection.")
+                break
+        client_socket.close()
+        receive_thread.join()
+    except ConnectionRefusedError:
+        print("Server didn't even exist")
+        client_socket.close()
 
 def receive_messages(client_socket):
     while True:
@@ -31,9 +34,9 @@ def receive_messages(client_socket):
             response = client_socket.recv(1024).decode('utf-8')
             print(response)
 
-        except ConnectionAbortedError:
-            print("Connection to the server closed.")
-            break
+        except ConnectionResetError:
+            print("Server is somewhat unexpectedly closed.")
+            return 0
         
 if __name__ == '__main__':
     client_program()
