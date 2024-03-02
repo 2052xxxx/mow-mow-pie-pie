66d196c804f1fc0dccc1109fcbaeb85bb89df546
2052xxxx <20522039@gm.uit.edu.vn>
2024-03-02 18:09:50 +0700

    no update haizz

--- chapter2/chapter2_P-2.35/server.py
+++ chapter2/chapter2_P-2.35/server.py
@@ -25,46 +25,59 @@ class ThreadedServer():
                 clients.add(conn)
             print(f"connected by {address[0]}:{address[1]}")
 
+    def sendAll(self, conn, message):
+        with clients_lock:
+            for c in clients:
+                if c != conn:
+                    c.sendall(message.encode('utf-8'))
+
     def listenToClient(self, conn, address):
         # threading.current_thread().ident - ko có xóaaaaaa
         conn.send(str.encode('Welcome to the Server!'))
-        while True:
-            data = conn.recv(1024)
-            if not data or data.decode('utf-8')=='bye':
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data or data.decode('utf-8')=='bye':
+                    if address not in self._clients:
+                        message = f"port {address[1]} has left the chat"
+                        print(message)
+                    else:
+                        message = f"{self._clients[address]} has left the chat"
+                        print(message)
+                        del self._clients[address]
+                    self.sendAll(conn, message)
+                    with clients_lock:
+                        clients.remove(conn)
+                    break
+
+                if data.decode('utf-8').lower().split()[0] == 'name':
+                    name = ' '.join(data.decode('utf-8').split()[1:])
+                    self._clients[address] = name
+                
                 if address not in self._clients:
-                    message = f"port {address[1]} leaves the chat"
-                    print(message)
+                    message = f"port {address[1]}: {data.decode('utf-8')}"
                 else:
-                    message = f"{self._clients[address]} leaves the chat"
-                    print(message)
-                    del self._clients[address]
-                with clients_lock:
-                    for c in clients:
-                        if c != conn:
-                            c.sendall(message.encode('utf-8'))
-                with clients_lock:
-                    clients.remove(conn)
-                break
-
-            if data.decode('utf-8').lower().split()[0] == 'name':
-                name = ' '.join(data.decode('utf-8').split()[1:])
-                self._clients[address] = name
-            
+                    message = f"'{self._clients[address]}': {data.decode('utf-8')}"
+                print(message )
+                self.sendAll(conn, message)
+                
+        except socket.error:
             if address not in self._clients:
-                message = f"Port {address[1]}: {data.decode('utf-8')}"
+                message = f"port {address[1]} has left the chat"
+                print(message)
             else:
-                message = f"'{self._clients[address]}': {data.decode('utf-8')}"
-            print(message )
-
+                message = f"{self._clients[address]} has left the chat"
+                print(message)
+                del self._clients[address]
+            self.sendAll(conn, message)
             with clients_lock:
-                for c in clients:
-                    if c != conn:
-                        c.sendall(message.encode('utf-8'))
+                clients.remove(conn)
         conn.close()
         
-        # def privateChat(self, conn)
-def main():
-    ThreadedServer('127.0.0.1', 9999).listen()
+
+    def privateChat(self, conn):
+        pass
 
 if __name__ == "__main__":
-    main()
+    ThreadedServer('127.0.0.1', 9999).listen()
+
